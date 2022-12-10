66bbd4812cd3ca86458dee48c823543159b811e3
Fa_ishal21 <faisal2017bahi@gmail.com>
2022-12-10 21:31:23 +0700

    validasi dan insert post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use App\Models\Kategori;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
+
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     //  protected $fillable = ['title', 'excerpt', 'body'];
 
@@ -49,5 +51,18 @@ class Post extends Model
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
